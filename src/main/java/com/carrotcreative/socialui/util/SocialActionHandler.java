983fd2c276e10a9e29fab2b722468eb0599527b2
Kaushik Saurabh <kaushik@carrotcreative.com>
2015-06-19 14:40:32 -0400

    Make mention and hashtags less greedy.
    Add method for email.
    Make Pattern.compile as static patterns.

--- src/main/java/com/carrotcreative/socialui/util/SocialActionHandler.java
+++ src/main/java/com/carrotcreative/socialui/util/SocialActionHandler.java
@@ -3,5 +3,6 @@ package com.carrotcreative.socialui.util;
 public interface SocialActionHandler {
     public void handleHashtag(String hashtag);
     public void handleMention(String mention);
+    public void handleEmail(String email);
     public void handleUrl(String url);
 }
